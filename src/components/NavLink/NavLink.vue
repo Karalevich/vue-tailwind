a49ffab27842342d41a9cf636fa0a0c440d19dc2
Karalevich <karalevichandrei@gmail.com>
2024-07-21 18:56:19 -0700

    first commit

---
+++ src/components/NavLink/NavLink.vue
@@ -0,0 +1,16 @@
+<template>
+  <router-link
+    :to="to"
+    class="dark:text-slate-200 dark:hover:text-purple-500 dark:hover:border-purple-500 transition duration-300 inline-block py-3 px-2 border-b-2 border-transparent hover:text-purple-800 hover:border-purple-800 text-gray-500 font-semibold"
+  >
+    <slot></slot>
+  </router-link>
+</template>
+
+<script setup>
+defineProps({
+  to: String
+})
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/components/TheHeader/TheHeader.vue
@@ -0,0 +1,42 @@
+<template>
+  <header class="px-2 border-b flex items-center justify-between h-14">
+    <a class="dark:text-purple-500 uppercase font-bold text-logo-main" href="#">SearchStax</a>
+    <nav class="hidden md:flex items-center">
+      <ul class="inline-flex items-center">
+        <li><NavLink to="/">Home</NavLink></li>
+        <li><NavLink to="about">About</NavLink></li>
+      </ul>
+      <ul class="inline-flex items-center">
+        <li>
+          <HeaderButton>Login</HeaderButton>
+        </li>
+        <li>
+          <HeaderButton>Register</HeaderButton>
+        </li>
+      </ul>
+    </nav>
+    <button class="inline-block md:hidden text-gray-600">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="h-6 w-6"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M4 6h16M4 12h16M4 18h16"
+        />
+      </svg>
+    </button>
+  </header>
+</template>
+
+<script setup>
+import NavLink from '@/components/NavLink/NavLink.vue'
+import HeaderButton from '@/components/HeaderButton/HeaderButton.vue'
+</script>
+
+<style scoped lang="scss"></style>
